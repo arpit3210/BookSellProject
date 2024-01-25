953afd5a44682b3dcf303e1f5007217631e35ba0
1234 <arpitsinghthakur321@gmail.com>
2024-01-26 01:15:24 +0530

    Added Infinite Carousel Mode for Reviewcards

--- my-project/src/Components/Testimonial.jsx
+++ my-project/src/Components/Testimonial.jsx
@@ -29,7 +29,57 @@ const Testimonial = () => {
      
      <h1 className="text-center font-semibold text-5xl py-16 font-sans text-[#f7644c]  ">Reviews</h1>
      
-      <Carousel responsive={responsive}>
+      <Carousel  additionalTransfrom={0}
+  arrows
+  autoPlaySpeed={3000}
+  centerMode={false}
+  className=""
+  containerClass="container-with-dots"
+  dotListClass=""
+  draggable
+  focusOnSelect={false}
+  infinite
+  itemClass=""
+  keyBoardControl
+  minimumTouchDrag={80}
+  pauseOnHover
+  renderArrowsWhenDisabled={false}
+  renderButtonGroupOutside={false}
+  renderDotsOutside={false}
+  responsive={{
+    desktop: {
+      breakpoint: {
+        max: 3000,
+        min: 1024
+      },
+      items: 3,
+      partialVisibilityGutter: 40
+    },
+    mobile: {
+      breakpoint: {
+        max: 464,
+        min: 0
+      },
+      items: 1,
+      partialVisibilityGutter: 30
+    },
+    tablet: {
+      breakpoint: {
+        max: 1024,
+        min: 464
+      },
+      items: 2,
+      partialVisibilityGutter: 30
+    }
+  }}
+  rewind={false}
+  rewindWithAnimation={false}
+  rtl={false}
+  shouldResetAutoplay
+  showDots={false}
+  sliderClass=""
+  slidesToSlide={1}
+  swipeable >
         {reviews.map((review) => (
           <ReviewCards
             key={review.customerName}
